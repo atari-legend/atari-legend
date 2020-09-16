4e244c080260a3557fba8afe3cb95a1d1dc6d423
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-09-16 17:14:42 +0200

    Use pluralization helper for the count of online users

--- resources/views/layouts/online_users.blade.php
+++ resources/views/layouts/online_users.blade.php
@@ -1,6 +1,6 @@
 @if (request()->attributes->has('onlineUsers'))
     <div class="container-fluid text-center bg-darklight m-0 px-0">
-        <p class="my-1 pt-1">Currently {{ request()->attributes->get('onlineUsers')->count() }} registered users online</p>
+        <p class="my-1 pt-1">Currently {{ request()->attributes->get('onlineUsers')->count() }} registered {{ Str::plural('user', request()->attributes->get('onlineUsers')->count()) }} online</p>
         <ul class="list-inline my-0 p-1 bg-primary">
             @foreach (request()->attributes->get('onlineUsers') as $user)
                 <li class="list-inline-item"><i class="far fa-user"></i> {{ $user->userid }}</li>
@@ -11,7 +11,7 @@
 
 @if (request()->attributes->has('pastDayUsers'))
     <div class="container-fluid text-center bg-darklight mt-0 mb-3 px-0">
-        <p class="my-1 pt-1">In the past 24h there were {{ request()->attributes->get('pastDayUsers')->count() }} registered users online</p>
+        <p class="my-1 pt-1">In the past 24h there were {{ request()->attributes->get('pastDayUsers')->count() }} registered {{ Str::plural('user', request()->attributes->get('pastDayUsers')->count()) }} online</p>
         <ul class="list-inline my-0 p-1 bg-primary">
             @foreach (request()->attributes->get('pastDayUsers') as $user)
                 <li class="list-inline-item"><i class="far fa-user"></i> {{ $user->userid }}</li>
