fb900270c6f6d61be560a67ea1ea7e3327f8b8bf
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-09-16 16:52:39 +0200

    Implement Links page

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,10 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Website;
+use App\WebsiteCategory;
+use App\WebsiteValidate;
+use Illuminate\Http\Request;
+
 class LinkController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('links.index');
+        $category = $request->filled('category')
+            ? WebsiteCategory::find($request->category)
+            : null;
+
+        $categories = WebsiteCategory::select()
+            ->orderBy('website_category_name')
+            ->get();
+
+        $websites = Website::select();
+
+        if ($category !== null) {
+            $websites->join('website_category_cross', 'website_category_cross.website_id', '=', 'website.website_id')
+                ->where('website_category_id', $category->website_category_id);
+        }
+
+        $websites = $websites
+            ->orderBy('website_name')
+            ->paginate(5);
+
+        return view('links.index')
+            ->with([
+                'categories'    => $categories,
+                'category'      => $category,
+                'websites'      => $websites,
+            ]);
+    }
+
+    public function postLink(Request $request)
+    {
+        $submission = new WebsiteValidate();
+        $submission->website_name = $request->name;
+        $submission->website_url = $request->url;
+        $submission->website_description = $request->description;
+        $submission->website_date = time();
+
+        $request->user()->websiteSubmissions()->save($submission);
+
+        $request->session()->flash('alert-title', 'Website submitted');
+        $request->session()->flash(
+            'alert-success',
+            'Thanks for your submission, a moderator will review it soon!'
+        );
+
+        return back();
     }
 }
--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Links</h1>
+    <div class="row">
+        <div class="col-3 pl-0">
+            @include('links.card_submit')
+            <x-cards.a-l-mobile />
+        </div>
+        <div class="col-6">
+            @include('links.card_links')
+        </div>
+        <div class="col-3 pr-0">
+            <x-cards.screenstar />
+            <x-cards.trivia />
+        </div>
+    </div>
 @endsection
