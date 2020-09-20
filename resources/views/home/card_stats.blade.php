22f932bdb0ac17ab9e23271e9317b8025d27c128
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-09-20 12:08:11 +0200

    Fix invalid HTML (UL inside P)
    Also made the `striped` class apply to all element types

--- resources/views/home/card_stats.blade.php
+++ resources/views/home/card_stats.blade.php
@@ -2,13 +2,11 @@
     <div class="card-header text-center">
         <h2 class="text-uppercase">Statistics</h2>
     </div>
-    <div class="card-body p-2">
-        <p class="card-text">
-            <ul>
-                @foreach ($stats as $key => $stat)
-                    <li>{{ $key }}: {{ $stat }}
-                @endforeach
-            </ul>
-        </p>
+    <div class="card-body p-0">
+        <ul class="p-0 list-unstyled striped">
+            @foreach ($stats as $key => $stat)
+                <li class="px-2 py-1">{{ $key }}: {{ $stat }}
+            @endforeach
+        </ul>
     </div>
 </div>
