03b6878fd72700904bf260fb8a5152efd2e67768
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-09-22 21:58:40 +0200

    Initial work on the menu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class= row mb-0">
+                        <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,44 +1,44 @@
-<nav class="navbar navbar-expand-md navbar-light bg-primary mb-4">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-md navbar-light bg-primary mb-4 p-0">
+    <div class="container-fluid pr-0">
         <a class="navbar-brand" href="{{ route('home.index') }}"><i class="fas fa-home"></i></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('news.*') ? 'active' : '' }}" href="{{ route('news.index') }}">News</a>
+            <ul class="navbar-nav navbar-main mr-auto">
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('news.*') ? 'active' : '' }}" href="{{ route('news.index') }}">News</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('games.*') ? 'active' : '' }}" href="{{ route('games.index') }}">Games</a>
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('games.*') ? 'active' : '' }}" href="{{ route('games.index') }}">Games</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('reviews.*') ? 'active' : '' }}" href="{{ route('reviews.index') }}">Reviews</a>
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('reviews.*') ? 'active' : '' }}" href="{{ route('reviews.index') }}">Reviews</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('interviews.*') ? 'active' : '' }}" href="{{ route('interviews.index') }}">Interviews</a>
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('interviews.*') ? 'active' : '' }}" href="{{ route('interviews.index') }}">Interviews</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('articles.*') ? 'active' : '' }}" href="{{ route('articles.index') }}">Articles</a>
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('articles.*') ? 'active' : '' }}" href="{{ route('articles.index') }}">Articles</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('links.*') ? 'active' : '' }}" href="{{ route('links.index') }}">Links</a>
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('links.*') ? 'active' : '' }}" href="{{ route('links.index') }}">Links</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::routeIs('about.*') ? 'active' : '' }}" href="{{ route('about.index') }}">About</a>
+                <li class="nav-item p-0">
+                    <a class="text-dark nav-link py-3 px-2 px-lg-3 {{ Request::routeIs('about.*') ? 'active' : '' }}" href="{{ route('about.index') }}">About</a>
                 </li>
             </ul>
-            <form class="d-flex mr-3" method="get" action="{{ route('games.search') }}">
-                <input class="form-control mr-2" name="title" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-dark" type="submit"><i class="fas fa-search"></i></button>
+            <form class="d-flex search" method="get" action="{{ route('games.search') }}">
+                <input class="form-control bg-black" name="title" type="search" placeholder="Search" aria-label="Search" required>
+                <button class="btn" type="submit"><i class="fas fa-search"></i></button>
             </form>
-            <ul class="navbar-nav">
+            <ul class="navbar-nav navbar-profile">
                 @auth
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                     @csrf
                 </form>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="user-menu" role="button" data-toggle="dropdown" aria-expanded="false">
+                <li class="nav-item dropdown p-0">
+                    <a class="text-dark nav-link dropdown-toggle py-3 px-2 px-lg-3" href="#" id="user-menu" role="button" data-toggle="dropdown" aria-expanded="false">
                         @if (Auth::user()->avatar_ext)
                             <img class="rounded-circle mr-1" height="30" src="{{ asset('storage/images/user_avatars/'.Auth::user()->user_id.'.'.Auth::user()->avatar_ext) }}" alt="User avatar">
                         @endif
@@ -57,14 +57,18 @@
                 </li>
                 @endauth
                 @guest
-                    <li class="nav-item">
-                        <a class="text-dark nav-link {{ Request::routeIs('login') ? 'active' : '' }}" href="{{ route('login') }}">Log-in</a>
-                    </li>
                     @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="text-dark nav-link {{ Request::routeIs('register') ? 'active' : '' }}" href="{{ route('register') }}">Register</a>
+                    <li class="nav-item p-0">
+                        <a class="text-dark nav-link py-3 {{ Request::routeIs('register') ? 'active' : '' }}" href="{{ route('register') }}" title="Register">
+                            <i class="fas fa-user-plus"></i>
+                        </a>
                     </li>
                     @endif
+                    <li class="nav-item p-0">
+                        <a class="text-dark nav-link py-3 {{ Request::routeIs('login') ? 'active' : '' }}" href="{{ route('login') }}" title="Log in">
+                            <i class="fas fa-sign-in-alt"></i>
+                        </a>
+                    </li>
                 @endguest
             </ul>
         </div>
