3579c91182ca4fa661c53c0acc559dec19391b4f
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-09-26 18:20:35 +0200

    Added Google Analytics tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,18 @@
             ]
         }
     </script>
+
+    @if (env('GOOGLE_ANALYTICS_ID'))
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="//www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
+        </script>
+    @endif
 </head>
 
 <body>
