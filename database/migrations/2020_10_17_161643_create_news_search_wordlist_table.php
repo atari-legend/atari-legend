8f1b942de3a14cb26f1aa2cbe1ccc71b574080bc
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-10-18 16:30:40 +0200

    Fix news_search_wordlist table auto-increment column
    Laravel Migrations does not support setting a non-primary key
    auto-increment, so use a raw SQL statement for now.

--- database/migrations/2020_10_17_161643_create_news_search_wordlist_table.php
+++ database/migrations/2020_10_17_161643_create_news_search_wordlist_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateNewsSearchWordlistTable extends Migration
@@ -14,9 +15,16 @@ class CreateNewsSearchWordlistTable extends Migration
     public function up()
     {
         Schema::create('news_search_wordlist', function (Blueprint $table) {
-            $table->mediumIncrements('news_word_id');
-            $table->string('news_word_text', 50)->default('')->unique();
+            $table->mediumInteger('news_word_id');
+            $table->string('news_word_text', 50)->default('')->primary();
         });
+
+        // FIXME: Implement for other drivers?
+        if (DB::connection()->getDriverName() === 'mysql') {
+            // Add auto-increment to the ID column
+            DB::statement("ALTER TABLE `news_search_wordlist` ADD KEY `news_word_id` (`news_word_id`)");
+            DB::statement("ALTER TABLE `news_search_wordlist` MODIFY `news_word_id` mediumint AUTO_INCREMENT");
+        }
     }
 
     /**
