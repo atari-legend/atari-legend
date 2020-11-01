82cf424024baf04494165a7bc64701ea7aadaaa7
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-11-01 13:12:24 +0100

    For #14: More adjustement of reviews list

--- resources/views/reviews/card_author.blade.php
+++ resources/views/reviews/card_author.blade.php
@@ -4,25 +4,24 @@
     </div>
 
     <div class="card-body p-0 bg-darklight">
-        <p class="card-text p-2 pb-0">
+        <p class="card-text p-2 mb-0">
             @if (isset($mode) && $mode === 'submit' && $reviews->isEmpty())
                 You haven't written any reviews before. This is your first. Congratulations and good luck!
                 For some examples, see the card 'Indepth reviews'.
             @else
                 {{ Helper::user($user) }} has written {{ $reviews->count() }} additional reviews
             @endif
-
-            @if ($reviews->isNotEmpty())
-                <ul class="striped list-unstyled">
-                    @foreach ($reviews as $r)
-                        <li class="p-1 pl-4">
-                            <a href="{{ route('reviews.show', ['review' => $r ]) }}">{{ $r->games->first()->game_name }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            @endif
         </p>
 
+        @if ($reviews->isNotEmpty())
+            <ul class="striped mb-0 list-unstyled">
+                @foreach ($reviews as $r)
+                    <li class="p-1 pl-4">
+                        <a href="{{ route('reviews.show', ['review' => $r ]) }}">{{ $r->games->first()->game_name }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        @endif
 
     </div>
 </div>
