5cc4ac35df1df21cb2014804ada455d674797a41
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-11-03 17:58:37 +0100

    Fixes #18: AL TV YouTube link

--- resources/views/home/card_altv.blade.php
+++ resources/views/home/card_altv.blade.php
@@ -1,12 +1,12 @@
 <div class="card bg-dark mb-4">
     <div class="card-header text-center">
-        <h2 class="text-uppercase"><a href="https://www.youtube.com/playlist?list=PLKg-kyPuSwaPpzIJph9K06CLg9mZtB_rX">Atari Legend TV</a></h2>
+        <h2 class="text-uppercase"><a href="https://www.youtube.com/atarilegend">Atari Legend TV</a></h2>
     </div>
     <div class="card-body p-0">
         <img class="w-100" src="{{ asset('images/cards/AL%20TV.png') }}" alt="AL TV logo">
         <p class="card-text p-2">
             Check out the ATARI LEGEND YouTube show featuring all the goodies you have come to love from team AL, but now on tele!
-            <a class="d-block text-right" href="https://www.youtube.com/playlist?list=PLKg-kyPuSwaPpzIJph9K06CLg9mZtB_rX">
+            <a class="d-block text-right" href="https://www.youtube.com/atarilegend">
                 More <i class="fas fa-chevron-right"></i>
             </a>
         </p>
