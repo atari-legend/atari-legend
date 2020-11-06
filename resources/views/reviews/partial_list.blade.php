6528a57f3eaeb14ba5b9ceadd8fa6abb632443b4
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-11-06 18:36:19 +0100

    For #20: Try to wrap the text around the screenshot for reviews

--- resources/views/reviews/partial_list.blade.php
+++ resources/views/reviews/partial_list.blade.php
@@ -1,24 +1,27 @@
-<div class="row g-0 p-2">
-    <div class="col-4 pr-2 align-self-center lightbox-gallery">
+<div class="p-2 lightbox-gallery">
+    <div class="clearfix mb-2">
+        <h3 class="text-h4">
+            <a href="{{ route('reviews.show', ['review' => $review]) }}">
+                {{ $review->games->first()->game_name}}
+            </a>
+        </h3>
+        <p class="card-subtitle text-muted">{{ date('F j, Y', $review->review_date) }} by {{ Helper::user($review->user) }}</p>
+    </div>
+
+    <div class="clearfix">
         @if ($review->screenshots->isNotEmpty())
             @php
                 $screenshot = $review->screenshots->random()->screenshot
             @endphp
             <a class="lightbox-link" href="{{ asset('storage/images/game_screenshots/'.$screenshot->file) }}">
-                <img class="w-100" src="{{ asset('storage/images/game_screenshots/'.$screenshot->file) }}" alt="Screenshot of {{ $review->games->first()->game_name }}">
+                <img class="col-4 col-sm-3 float-left mt-1 mr-2 mb-1" src="{{ asset('storage/images/game_screenshots/'.$screenshot->file) }}" alt="Screenshot of {{ $review->games->first()->game_name }}">
             </a>
         @endif
-    </div>
-    <div class="col-8 pl-2">
-        <h3>
-            <a href="{{ route('reviews.show', ['review' => $review]) }}">
-                {{ $review->games->first()->game_name}}
-            </a>
-        </h3>
-        <p class="card-subtitle text-muted mb-2">{{ date('F j, Y', $review->review_date) }} by {{ Helper::user($review->user) }}</p>
+
         {!! Helper::bbCode(Helper::extractTag($review->review_text, "frontpage")) !!}<br>
         <a class="d-block text-right" href="{{ route('reviews.show', ['review' => $review]) }}">
             More <i class="fas fa-chevron-right"></i>
         </a>
     </div>
+
 </div>
