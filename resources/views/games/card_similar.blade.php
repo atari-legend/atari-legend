4c6699867c7d033e9e983f480e4c13e14b3a4776
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-11-14 18:07:19 +0100

    Fixes #51: Added figure + caption to the similar game card

--- resources/views/games/card_similar.blade.php
+++ resources/views/games/card_similar.blade.php
@@ -7,9 +7,21 @@
             <h2 class="text-uppercase">Similar Game</h2>
         </div>
         <div class="card-body p-0">
-            <a href="{{ route('games.show', ['game' => $similar]) }}">
-                <img class="w-100" src="{{ asset('storage/images/game_screenshots/'.$similar->screenshots[0]->file) }}" alt="Screenshot of {{ $similar->game_name }}">
-            </a>
+            <figure>
+                <img class="w-100 pixelated" src="{{ asset('storage/images/game_screenshots/'.$similar->screenshots[0]->file) }}" alt="Screenshot of {{ $similar->game_name }}">
+                <figcaption class="py-2 px-3">
+                    <div class="figcaption-caret"><i class="fas fa-angle-up fa-2x"></i></div>
+                    <div class="figcaption-title"><a href="{{ route('games.show', ['game' => $similar]) }}">{{ $similar->game_name }}</a></div>
+                    @if ($similar->releases->isNotEmpty())
+                        <div class="figcaption-note">
+                            <a href="{{ route('games.search', ['year' => $similar->releases->first()->year]) }}">{{ $similar->releases->first()->year }}</a>
+                        </div>
+                    @endif
+                    @if ($similar->developers->isNotEmpty())
+                        <div class="figcaption-subtitle mb-2"><strong>{{ $similar->developers->first()->pub_dev_name }}</strong></div>
+                    @endif
+                </figcaption>
+            </figure>
         </div>
     </div>
 @endif
