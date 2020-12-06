8746671724cb218cb08f9b341637ff80b5938d10
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-12-06 17:38:22 +0100

    StyleCI issues

--- config/captcha.php
+++ config/captcha.php
@@ -3,14 +3,14 @@
  * Secret key and Site key get on https://dashboard.hcaptcha.com/sites
  * */
 return [
-    'secret' => env('CAPTCHA_SECRET'),
-    'sitekey' => env('CAPTCHA_SITEKEY', 'ce638d28-bb25-498f-aabe-db003adaa817'),
+    'secret'      => env('CAPTCHA_SECRET'),
+    'sitekey'     => env('CAPTCHA_SITEKEY', 'ce638d28-bb25-498f-aabe-db003adaa817'),
     // \GuzzleHttp\Client used is the default client
     'http_client' => \Buzz\LaravelHCaptcha\HttpClient::class,
-    'options' => [
-        'multiple' => false
+    'options'     => [
+        'multiple' => false,
     ],
     'attributes' => [
-        'data-theme' => 'dark'
+        'data-theme' => 'dark',
     ],
 ];
