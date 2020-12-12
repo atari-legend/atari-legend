e85955c8660df0b6253bb54f6c94d2d39740fef6
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-12-12 16:39:30 +0100

    Remove legacy site cookie support
    Since we run the legacy site on a separate domain we cannot set a cookie
    for it.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,13 +10,6 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /**
-     * Name of the 'remember me' cookie used by the legacy site
-     * We set it so that logging in to the new site also logs in
-     * to the legacy site.
-     */
-    const LEGACY_SITE_COOKIE_NAME = 'cooksession';
-
     /*
     |--------------------------------------------------------------------------
     | Login Controller
@@ -55,40 +48,4 @@ class LoginController extends Controller
         return 'userid';
     }
 
-    /**
-     * The user has been authenticated.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param mixed                    $user
-     *
-     * @return mixed
-     */
-    protected function authenticated(Request $request, $user)
-    {
-        // Set a cookie used to automatically login in the legacy site
-        $cookieValue = bin2hex(random_bytes(16));
-        // Do not use the Laravel Cookie facade as it encrypts
-        // the cookie, and we need to set the raw value
-        setcookie(LoginController::LEGACY_SITE_COOKIE_NAME, $cookieValue, time() + 60 * 60 * 24 * 100);
-
-        // Update current session for that user in the DB, used
-        // by the legacy site
-        User::where('user_id', $user->user_id)
-            ->update(['session' => $cookieValue]);
-    }
-
-    /**
-     * The user has logged out of the application.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return mixed
-     */
-    protected function loggedOut()
-    {
-        // Remove session cookie used by the legacy site
-        if (isset($_COOKIE[LoginController::LEGACY_SITE_COOKIE_NAME])) {
-            setcookie(LoginController::LEGACY_SITE_COOKIE_NAME, null, -1);
-        }
-    }
 }
