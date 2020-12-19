cf217daf83b6dbff72d9716f2d8a69e4ba2e4c2b
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2020-12-19 12:41:29 +0100

    Improve display of games missing screenshots in search results

--- resources/views/games/card_search_results.blade.php
+++ resources/views/games/card_search_results.blade.php
@@ -85,6 +85,15 @@
                         <a href="{{ route('games.show', ['game' => $game]) }}">
                             <img class="w-100 mb-2 bg-dark" src="{{ asset('storage/images/game_screenshots/'.$game->screenshots->random()->file) }}" alt="Screenshot of {{ $game->game_name }}">
                         </a>
+                    @else
+                        <div class="text-center position-relative">
+                            <div class="position-absolute text-center w-100 text-muted mt-3">
+                                n/a
+                            </div>
+                            <a href="{{ route('games.show', ['game' => $game]) }}">
+                                <img class="w-100 mb-2" src="{{ asset('images/32x20.png') }}">
+                            </a>
+                        </div>
                     @endif
 
                     <a href="{{ route('games.show', ['game' => $game]) }}">{{ $game->game_name }}</a><br>
