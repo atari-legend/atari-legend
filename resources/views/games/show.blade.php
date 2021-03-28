03fcc5dd26063f99cc46e5cdebf8a0350e7f67e3
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2021-03-28 15:29:45 +0200

    For #102: Try out a different layout for the game page

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -5,22 +5,22 @@
 @section('content')
     <h1 class="visually-hidden">{{ $game->game_name }}</h1>
     <div class="row">
-        <div class="col-12 col-sm-6 col-lg-3 order-2 order-lg-1 lightbox-gallery">
+        <div class="col-12 col-sm-6 col-lg-3 lightbox-gallery">
             @include('games.card_gameinfo')
+            @include('games.card_boxscan')
             @include('games.card_series')
-            @include('games.card_releases')
-            @include('games.card_comments')
         </div>
-        <div class="col-12 col-lg-6 order-1 order-lg-2">
+        <div class="col-12 col-sm-6">
             @include('games.card_screenshots')
             @include('games.card_reviews')
             @include('games.card_menus')
         </div>
-        <div class="col col-sm-6 col-lg-3 order-3">
-            @include('games.card_boxscan')
+        <div class="col col-sm-6 col-lg-3">
+            @include('games.card_releases')
             @include('games.card_similar')
             @include('games.card_facts')
             @include('games.card_interviews')
+            @include('games.card_comments')
             @include('games.card_submit_info')
         </div>
     </div>
