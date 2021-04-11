866717f2db62d11588279ae979ed9f7cb04843f0
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2021-04-11 12:06:49 +0200

    For #102: Review game page tile order once more

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -7,9 +7,8 @@
     <div class="row">
         <div class="col-12 col-sm-6 col-lg-3 lightbox-gallery">
             @include('games.card_gameinfo')
-            @include('games.card_releases')
             @include('games.card_series')
-            @include('games.card_comments')
+            @include('games.card_boxscan')
         </div>
         <div class="col-12 col-lg-6 order-sm-3 order-lg-2">
             @include('games.card_screenshots')
@@ -17,10 +16,11 @@
             @include('games.card_menus')
         </div>
         <div class="col col-sm-6 col-lg-3 order-sm-2 order-lg-3">
-            @include('games.card_boxscan')
+            @include('games.card_releases')
             @include('games.card_similar')
             @include('games.card_facts')
             @include('games.card_interviews')
+            @include('games.card_comments')
             @include('games.card_submit_info')
         </div>
     </div>
