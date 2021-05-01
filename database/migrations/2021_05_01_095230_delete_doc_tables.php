677bf8166425088a93219e18e9e7b54e96d0d059
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2021-05-01 11:56:17 +0200

    Also remove doc tables that were used with the old menu structure

---
+++ database/migrations/2021_05_01_095230_delete_doc_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteDocTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ([
+            'doc',
+            'doc_category',
+            'doc_disk_game',
+            'doc_disk_tool',
+            'doc_game',
+            'doc_type',
+        ] as $table) {
+            Schema::drop($table);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
