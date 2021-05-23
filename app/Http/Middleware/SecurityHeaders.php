73ff937ced4d3695d9b8d03f0b6c425d7d75aa79
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2021-05-23 22:44:01 +0200

    Fix 'media-src' in Content Security Policy

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -27,7 +27,7 @@ class SecurityHeaders
         $csp = collect([
             "default-src 'self' matomo.atarilegend.com hcaptcha.com *.hcaptcha.com 'unsafe-inline' 'unsafe-eval' data:",
             "img-src 'self' data: *.ytimg.com",
-            'media-src sndhrecord.atari.org',
+            'media-src http://sndhrecord.atari.org',
         ]);
 
         $response->headers->set(
