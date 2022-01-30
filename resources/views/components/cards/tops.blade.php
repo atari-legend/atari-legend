8dbf2cda5243404016ff5e3a4a29226481cc6557
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2022-01-30 11:45:32 +0100

    Reformat "tops" card with tables

--- resources/views/components/cards/tops.blade.php
+++ resources/views/components/cards/tops.blade.php
@@ -8,57 +8,78 @@
 
             <div class="col p-2 border border-dark border-top-0">
                 <h3 class="fs-5 text-center">Developers</h3>
-                <ul class="list-unstyled">
-                    @foreach ($developers as $developer)
-                        <li>
-                            <a class="d-inline-block"
-                                href="{{ route('games.search', ['developer_id' => $developer->pub_dev_id]) }}">
-                                {{ $developer->pub_dev_name }}
-                            </a>: {{ $developer->game_count }} games
-                        </li>
-                    @endforeach
-                </ul>
+                <table class="table table-sm table-borderless">
+                    <tbody>
+                        @foreach ($developers as $developer)
+                            <tr>
+                                <td class="text-muted text-nowrap" style="width: 2rem">{{ $loop->index+1 }}.</td>
+                                <td class="text-nowrap" style="width: 45%;">
+                                    <a href="{{ route('games.search', ['developer_id' => $developer->pub_dev_id]) }}">
+                                        {{ $developer->pub_dev_name }}
+                                    </a>
+                                </td>
+                                <td>{{ $developer->game_count }} games</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
 
             <div class="col p-2 border border-dark border-top-0">
                 <h3 class="fs-5 text-center">Publishers</h3>
-                <ul class="list-unstyled">
-                    @foreach ($publishers as $publisher)
-                        <li>
-                            <a class="d-inline-block"
-                                href="{{ route('games.search', ['publisher_id' => $publisher->pub_dev_id]) }}">
-                                {{ $publisher->pub_dev_name }}
-                            </a>: {{ $publisher->release_count }} releases
-                        </li>
-                    @endforeach
-                </ul>
+                <table class="table table-sm table-borderless">
+                    <tbody>
+                        @foreach ($publishers as $publisher)
+                            <tr>
+                                <td class="text-muted text-nowrap" style="width: 2rem">{{ $loop->index+1 }}.</td>
+                                <td class="text-nowrap" style="width: 45%;">
+                                    <a href="{{ route('games.search', ['publisher_id' => $publisher->pub_dev_id]) }}">
+                                        {{ $publisher->pub_dev_name }}
+                                    </a>
+                                </td>
+                                <td>{{ $publisher->release_count }} games</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
 
             <div class="col p-2 border border-dark">
                 <h3 class="fs-5 text-center">Genres</h3>
-                <ul class="list-unstyled">
-                    @foreach ($genres as $genre)
-                        <li>
-                            <a class="d-inline-block" href="{{ route('games.search', ['genre_id' => $genre->id]) }}">
-                                {{ $genre->name }}
-                            </a>: {{ $genre->game_count }} games
-                        </li>
-                    @endforeach
-                </ul>
+                <table class="table table-sm table-borderless">
+                    <tbody>
+                        @foreach ($genres as $genre)
+                            <tr>
+                                <td class="text-muted text-nowrap" style="width: 2rem">{{ $loop->index+1 }}.</td>
+                                <td class="text-nowrap" style="width: 45%;">
+                                    <a href="{{ route('games.search', ['genre_id' => $genre->id]) }}">
+                                        {{ $genre->name }}
+                                    </a>
+                                </td>
+                                <td>{{ $genre->game_count }} games</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
 
             <div class="col p-2 border border-dark">
                 <h3 class="fs-5 text-center">Individuals</h3>
-                <ul class="list-unstyled">
-                    @foreach ($individuals as $individual)
-                        <li>
-                            <a class="d-inline-block"
-                                href="{{ route('games.search', ['individual_id' => $individual->ind_id]) }}">
-                                {{ $individual->ind_name }}
-                            </a>: {{ $individual->game_count }} games
-                        </li>
-                    @endforeach
-                </ul>
+                <table class="table table-sm table-borderless">
+                    <tbody>
+                        @foreach ($individuals as $individual)
+                            <tr>
+                                <td class="text-muted text-nowrap" style="width: 2rem">{{ $loop->index+1 }}.</td>
+                                <td class="text-nowrap" style="width: 45%;">
+                                    <a href="{{ route('games.search', ['individual_id' => $individual->ind_id]) }}">
+                                        {{ $individual->ind_name }}
+                                    </a>
+                                </td>
+                                <td>{{ $individual->game_count }} games</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
