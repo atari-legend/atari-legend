8572cfe6c6668e2f3547fb350145bf78aaedc151
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2022-08-21 22:29:48 +0200

    Tweak nav: Increase vertical padding

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,34 +7,34 @@
         <div class="collapse navbar-collapse" id="navbar">
             <ul class="navbar-nav navbar-main w-100">
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('news.*') ? 'active' : '' }}" href="{{ route('news.index') }}">News</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('news.*') ? 'active' : '' }}" href="{{ route('news.index') }}">News</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('games.*') ? 'active' : '' }}" href="{{ route('games.index') }}">Games</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('games.*') ? 'active' : '' }}" href="{{ route('games.index') }}">Games</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('menus.*') ? 'active' : '' }}" href="{{ route('menus.index') }}">Menus</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('menus.*') ? 'active' : '' }}" href="{{ route('menus.index') }}">Menus</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('reviews.*') ? 'active' : '' }}" href="{{ route('reviews.index') }}">Reviews</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('reviews.*') ? 'active' : '' }}" href="{{ route('reviews.index') }}">Reviews</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('interviews.*') ? 'active' : '' }}" href="{{ route('interviews.index') }}">Interviews</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('interviews.*') ? 'active' : '' }}" href="{{ route('interviews.index') }}">Interviews</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('articles.*') ? 'active' : '' }}" href="{{ route('articles.index') }}">Articles</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('articles.*') ? 'active' : '' }}" href="{{ route('articles.index') }}">Articles</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('links.*') ? 'active' : '' }}" href="{{ route('links.index') }}">Links</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('links.*') ? 'active' : '' }}" href="{{ route('links.index') }}">Links</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('magazines.*') ? 'active' : '' }}" href="{{ route('magazines.index') }}">Mags</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('magazines.*') ? 'active' : '' }}" href="{{ route('magazines.index') }}">Mags</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2" href="https://www.atari-forum.com/">Forum</a>
+                    <a class="text-dark nav-link py-3 px-2" href="https://www.atari-forum.com/">Forum</a>
                 </li>
                 <li class="nav-item flex-fill text-center p-0">
-                    <a class="text-dark nav-link py-2 px-2 {{ Request::routeIs('about.*') ? 'active' : '' }}" href="{{ route('about.index') }}">About</a>
+                    <a class="text-dark nav-link py-3 px-2 {{ Request::routeIs('about.*') ? 'active' : '' }}" href="{{ route('about.index') }}">About</a>
                 </li>
             </ul>
             <form class="search d-flex d-xl-none d-xxl-flex ps-2 py-0    justify-content-center" method="get" action="{{ route('games.search') }}">
@@ -52,7 +52,7 @@
                     @csrf
                 </form>
                 <li class="nav-item dropdown ps-2 text-center">
-                    <a class="text-dark nav-link dropdown-toggle py-2 @if (Auth::user()->avatar_ext) ps-0 @endif" href="#" id="user-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="text-dark nav-link dropdown-toggle py-3 @if (Auth::user()->avatar_ext) ps-0 @endif" href="#" id="user-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         @if (Auth::user()->avatar_ext)
                             <img id="avatar" class="rounded-circle border border-dark me-1" src="{{ asset('storage/images/user_avatars/'.Auth::user()->user_id.'.'.Auth::user()->avatar_ext) }}" alt="User avatar">
                         @endif
