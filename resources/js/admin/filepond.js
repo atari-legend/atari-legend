c658781c4c2419d2384abfc52256fd149f5caaef
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2023-05-07 12:25:17 +0200

    Refine bulk upload of release scans

--- resources/js/admin/filepond.js
+++ resources/js/admin/filepond.js
@@ -2,13 +2,16 @@ const filepond = require('filepond');
 const filePondPluginImagePreview = require('filepond-plugin-image-preview');
 const filePondPluginFileValidateType = require('filepond-plugin-file-validate-type');
 
+/**
+ * Setup Filepond drag'n drop file upload
+ */
 document.addEventListener('DOMContentLoaded', () => {
     filepond.registerPlugin(filePondPluginImagePreview);
     filepond.registerPlugin(filePondPluginFileValidateType);
 
     document.querySelectorAll('input.filepond').forEach(el => {
         var filetypes = el.dataset.filepondFiletypes || '';
-        filepond.create(el, {
+        var pond = filepond.create(el, {
             'acceptedFileTypes': filetypes.split(','),
             'chunkUploads': true,
             'server': {
@@ -18,5 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 patch: '?patch=',
             }
         });
+
+        // If a submit button selector was set in data attributes,
+        // enable it when all files have been processed (the first time)
+        var buttonSelector = el.dataset.filepondButton;
+        if (buttonSelector) {
+            var button = document.querySelector(buttonSelector);
+            if (button) {
+                pond.on('processfiles', () => {
+                    button.disabled = false;
+                });
+
+            }
+        }
     });
 });
--- resources/views/admin/games/games/releases/scans/card_upload.blade.php
+++ resources/views/admin/games/games/releases/scans/card_upload.blade.php
@@ -2,11 +2,16 @@
     <div class="card-body">
         <h2 class="card-title fs-4">Add scans</h2>
 
+        <p class="text-muted">
+            Add scans for the release (box, goodies, ...). After the upload you can change their types
+            and add notes.
+        </p>
+
         <form action="{{ route('admin.games.releases.scans.store', ['game' => $release->game, 'release' => $release]) }}"
             method="POST">
             @csrf
-            <input type="file" name="file[]" multiple class="filepond" data-filepond-filetypes="image/*">
-            <button class="btn btn-success">Upload all files</button>
+            <input type="file" name="file[]" multiple class="filepond" data-filepond-filetypes="image/*" data-filepond-button="#upload">
+            <button class="btn btn-success" id="upload" disabled>Upload all files</button>
         </form>
 
     </div>
