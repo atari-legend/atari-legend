8400803f95a2187b1bf0591effeeeaae74ca2a88
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2024-09-12 11:23:51 +0200

    Small optimization suggested by Google PageSpeed
    - Delete the duplicated `1.jpg` trivia card that was missed when
      converting the images to WebP
    - Lazy-load the YouTube thumbnails

--- resources/views/components/cards/altv-feed.blade.php
+++ resources/views/components/cards/altv-feed.blade.php
@@ -10,7 +10,7 @@
                     <div class="card-body p-0">
                         <div class="position-relative text-center w-100">
                             <a href="{{ $entry['link'] }}">
-                                <img class="w-100" src="{{ $entry['thumbnail'] }}" alt="Thumbnail of the video {{ $entry['title'] }}">
+                                <img class="w-100" src="{{ $entry['thumbnail'] }}" alt="Thumbnail of the video {{ $entry['title'] }}" loading="lazy">
                                 <img class="w-100 position-absolute top-0 start-0" src="{{ asset('images/play-overlay.png') }}" alt="Play button">
                             </a>
 
