978b3213847dc106104a536a8e8b2a041d26342f
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2024-09-12 12:17:48 +0200

    Fix StyleCI issues

--- config/livewire-tables.php
+++ config/livewire-tables.php
@@ -7,17 +7,17 @@ return [
     'theme' => 'bootstrap-5',
 
     /**
-     * Enable or Disable automatic injection of core assets
+     * Enable or Disable automatic injection of core assets.
      */
     'inject_core_assets_enabled' => false,
 
     /**
-     * Enable or Disable automatic injection of third-party assets
+     * Enable or Disable automatic injection of third-party assets.
      */
     'inject_third_party_assets_enabled' => false,
 
     /**
-     * Enable Blade Directives (Not required if automatically injecting or using bundler approaches)
+     * Enable Blade Directives (Not required if automatically injecting or using bundler approaches).
      */
     'enable_blade_directives' => true,
 ];
