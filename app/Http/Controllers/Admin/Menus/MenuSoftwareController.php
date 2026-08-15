4f1e376a1f9912b5914fc23319a6f1f9769c2334
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2026-08-15 11:20:17 +0200

    Fixes #265: Three menu controllers accept a nameless row

--- app/Http/Controllers/Admin/Menus/MenuSoftwareController.php
+++ app/Http/Controllers/Admin/Menus/MenuSoftwareController.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 
 class MenuSoftwareController extends Controller
 {
+    const VALIDATION_RULES = [
+        'name' => 'required',
+    ];
+
     public function index()
     {
         $softwares = MenuSoftware::orderBy('name')
@@ -44,6 +48,8 @@ class MenuSoftwareController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate(MenuSoftwareController::VALIDATION_RULES);
+
         $software = MenuSoftware::create([
             'name'                          => $request->name,
             'menu_software_content_type_id' => $request->type,
@@ -85,6 +91,8 @@ class MenuSoftwareController extends Controller
 
     public function update(Request $request, MenuSoftware $software)
     {
+        $request->validate(MenuSoftwareController::VALIDATION_RULES);
+
         $software->update([
             'name'                          => $request->name,
             'menu_software_content_type_id' => $request->type,
